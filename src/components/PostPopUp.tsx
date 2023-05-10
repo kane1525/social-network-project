9a6113056e7d1b3f9271a6898bbbf133b3f6e409
Nikita Malyeyev <kanekane1525@gmail.com>
2023-05-10 18:32:59 +0300

    fixed css styles

--- src/components/PostPopUp.tsx
+++ src/components/PostPopUp.tsx
@@ -67,7 +67,7 @@ function PostPopUp({ children, src }: PostPopUpProps) {
               borderRadius: "8px",
               boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
               zIndex: 999,
-              maxWidth: "90vw",
+              display: "flex",
             }}
             onClick={(e) => {
               e.stopPropagation();
