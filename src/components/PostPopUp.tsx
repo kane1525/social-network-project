da6688f6a92ead0c17f15b82db27c399be8dc70d
Nikita Malyeyev <kanekane1525@gmail.com>
2023-05-11 00:39:49 +0300

    styles bug v3

--- src/components/PostPopUp.tsx
+++ src/components/PostPopUp.tsx
@@ -68,6 +68,7 @@ function PostPopUp({ children, src }: PostPopUpProps) {
               boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
               zIndex: 999,
               display: "flex",
+              overflow: "auto",
             }}
             onClick={(e) => {
               e.stopPropagation();
